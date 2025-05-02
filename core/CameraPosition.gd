605cbd79f9cbe51279b3265416f395e67ba79366
Пригарин Олег Юрьевич <lolomap07@yandex.ru>
2025-05-02 17:26:32 +0300

    Control  objects

--- core/CameraPosition.gd
+++ core/CameraPosition.gd
@@ -4,7 +4,7 @@ var player: CharacterBody2D;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = get_node("../Player");
+	player = get_node("../Tilemap/Player");
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ core/draggable.gd
@@ -0,0 +1,30 @@
+extends RigidBody2D;
+
+const drag_force = 0.5;
+const rotate_back_speed = 10;
+
+var direction;
+
+var selected = false;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	if selected:
+		apply_force(Input.get_last_mouse_velocity() * drag_force);
+		#global_position = lerp(global_position, get_global_mouse_position(), drag_speed * delta);
+		
+
+
+func _on_interactable_area_input(viewport, event, shape_idx):
+	if G.Player.Manipulation == 0 && G.Player.Entity == 2 && Input.is_action_just_pressed("Click"):
+		selected = true;
+		
+func _input(event):
+	direction = get_global_mouse_position();
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+			selected = false;
